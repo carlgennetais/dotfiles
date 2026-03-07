2d32f54e9ff3727c44cf8722827a47d94e366035
Carl GENNETAIS <carl.gennetais@proton.me>
2026-03-08 00:53:12 +0100

    feat(fish): add basic aliases

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,32 @@
+set -gx EDITOR nvim
+
+alias v='nvim'
+alias gg='lazygit'
+
+# ls aliases
+if [ "$(command -v eza)" ]
+    alias ll='eza -la --icons=auto --group-directories-first'
+    alias l.='eza -d .*'
+    alias ls='eza'
+    alias l1='eza -1'
+end
+
+# ugrep for grep
+if [ "$(command -v ug)" ]
+    alias grep='ug'
+    alias egrep='ug -E'
+    alias fgrep='ug -F'
+    alias xzgrep='ug -z'
+    alias xzegrep='ug -zE'
+    alias xzfgrep='ug -zF'
+end
+
+# bat for cat
+alias cat='bat --style=plain --pager=never' 2>/dev/null
+
+if status is-interactive
+    # [ "$(command -v atuin)" ] && eval "$(atuin init fish $ATUIN_INIT_FLAGS)"
+    [ "$(command -v starship)" ] && eval "$(starship init fish)"
+    [ "$(command -v zoxide)" ] && eval "$(zoxide init fish)"
+    fzf --fish | source
+end
